12a965919c1f3514d8720ee9b0a3d41180a21338
DilmiKottahachchi <dilmi.kottahachchi@ica.se>
2025-10-12 17:02:19 +0200

    Organising code structure for collaboration

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from sklearn.datasets import load_diabetes
 
 app = FastAPI()
 
-
-@app.get("/predict")
-async def root():
-    return {"message": "Hello World"}
-
-    
+# Health check endpoint
 @app.get("/health")
-async def root():
-    return {"message": "Hello World"}
+async def health_check():
+    return {
+        "status": "ok",
+        "model_version": "0.1.0"
+    }
+
+# To predict if the patient needed follow ups
+@app.get("/predict")
+async def predict():
+    # Load the diabetes dataset
+    Xy = load_diabetes(as_frame=True)
+    X = Xy.frame.drop(columns=["target"])
+    y = Xy.frame["target"] # acts as a "progression index" (higher = worse)
+
+    return { "age": 0.02, "sex": -0.044, "bmi": 0.06, "bp": -0.03, "s1": -0.02, "s2": 0.03, "s3": -0.02, "s4": 0.02, "s5": 0.02, "s6": -0.001 }
